a40d82d7d779898bd5b00a0950492da2ab5791a8
Hector Cruz <hcruzgo@ed.ac.uk>
2024-10-11 12:05:40 +0100

    Add LXO mapping-based lidar=lidar calibration cfg

--- calibrators/sensor_calibration_manager/sensor_calibration_manager/calibrators/lxo/__init__.py
+++ calibrators/sensor_calibration_manager/sensor_calibration_manager/calibrators/lxo/__init__.py
@@ -1,5 +1,7 @@
 from .camera_lidar_tag_based_pnp_calibrator import CameraLidarTagBasedPNPCalibrator
+from .mapping_based_lidar_lidar_calibrator import MappingBasedLidarLidarCalibrator
 
 __all__ = [
     "CameraLidarTagBasedPNPCalibrator",
+    "MappingBasedLidarLidarCalibrator",
 ]

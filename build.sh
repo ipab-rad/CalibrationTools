27a52e66c06e226f27a0617f047d6ef6dc378a7b
Hector Cruz <hector_cruz95@live.com>
2025-07-15 18:57:27 +0100

    Simplify build.sh build cmd

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build --ssh default="$SSH_AUTH_SOCK" \
-    -t calibration_tools:latest-dev -f Dockerfile .
+docker build -t calibration_tools:latest-dev -f Dockerfile .
